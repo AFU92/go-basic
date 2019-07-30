6edd75aa38f50fc2b581c67de0e0d1bbe06504b3
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-07-29 22:21:38 -0500

    wip function operation

--- go-basic.go
+++ go-basic.go
@@ -4,14 +4,21 @@ import "fmt"
 
 func main() {
 
-	holaMundo()
+	var num1 float32 = 10
+	var num2 float32 = 6
 
-	holaMundo()
+	helloWorld()
 
-	holaMundo()
+	fmt.Print("\nThe sum is: ")
+
+	fmt.Println(operation(num1, num2, "+"))
 
 }
 
-func holaMundo() {
-	fmt.Println("Hola mundo!")
+func helloWorld() {
+	fmt.Println("Hello World!")
+}
+
+func operation(num1 float32, num2 float32, operator string) float32 {
+	return num1 + num2
 }
