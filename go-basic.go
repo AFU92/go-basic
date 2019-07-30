a73ac16ffb5c5741adc2be2325535e7c724117e0
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-07-29 22:54:01 -0500

    create calculator with functions

--- go-basic.go
+++ go-basic.go
@@ -6,16 +6,20 @@ func main() {
 
 	var num1 float32 = 10
 	var num2 float32 = 6
-	var operator string = "/"
+
+	var num3 int = 10
+	var num4 int = 6
 
 	helloWorld()
 
-	fmt.Println("Num1 y num2")
-	fmt.Println(num1, num2)
+	fmt.Println("\nNum1")
+	fmt.Println(num1)
 
-	fmt.Print("\nThe operation is " + operator + ":\n")
+	fmt.Println("\nNum2")
+	fmt.Println(num2)
 
-	fmt.Println(operation(num1, num2, operator))
+	calculator(num1, num2)
+	calculator2(num3, num4)
 
 }
 
@@ -39,4 +43,41 @@ func operation(num1 float32, num2 float32, operator string) float32 {
 	}
 
 	return result
+}
+
+func operation2(num3 int, num4 int, operator string) int {
+	var result int
+
+	if operator == "/" {
+		result = num3 / num4
+	}
+	if operator == "%" {
+		result = num3 % num4
+	}
+
+	return result
+}
+
+func calculator(num1 float32, num2 float32) {
+	fmt.Print("\nThe sum is: ")
+	fmt.Println(operation(num1, num2, "+"))
+
+	fmt.Print("The rest is: ")
+	fmt.Println(operation(num1, num2, "-"))
+
+	fmt.Print("The mult is: ")
+	fmt.Println(operation(num1, num2, "*"))
+
+	fmt.Print("The div float is: ")
+	fmt.Println(operation(num1, num2, "/"))
+
+}
+
+func calculator2(num3 int, num4 int) {
+
+	fmt.Print("The div int is: ")
+	fmt.Println(operation2(num3, num4, "/"))
+
+	fmt.Print("The residue is: ")
+	fmt.Println(operation2(num3, num4, "%"))
 }
