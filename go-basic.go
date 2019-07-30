8642092a382f30df13e7ec75cc0f32cbf058c280
Andrea Fuentes <andrea.fuentes@peiky.com>
2019-07-29 23:55:57 -0500

    order t-shirt

--- go-basic.go
+++ go-basic.go
@@ -6,27 +6,34 @@ func main() {
 
 	fmt.Println(returnText())
 
+	fmt.Println(tshirt(24, "€"))
+
 	fmt.Println(returnText2())
 
-	fmt.Println(tshirt(45))
+	fmt.Println(tshirt(45, "$"))
 }
 
-// recibe una variable de tipo int y devuelve un string y un int
-func tshirt(order int) (string, int) {
-	return "Quantity t-shit is: ", order
+// recibe una variable de tipo float32 y devuelve un string y un float32
+func tshirt(order float32, money string) (string, string, float32) {
+
+	price := func() float32 {
+		return order * 7
+	}
+
+	return "The total price is: ", money, price()
 }
 
 // Return 1
 func returnText() (string, string, int) {
-	data1 := "Default"
-	data2 := "text"
-	data3 := 100
+	data1 := "Order"
+	data2 := "No."
+	data3 := 1
 	return data1, data2, data3
 }
 
 // Return 2
 func returnText2() (data4 string, data5 int) {
-	data4 = "Default"
-	data5 = 200
+	data4 = "Order No."
+	data5 = 2
 	return
 }
